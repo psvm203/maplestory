67bdb9775039d7a5fd287b4068e1ea0ecabc1330
psvm203 <psvm203@proton.me>
2025-10-13 11:04:09 +0900

    feat: implement fetching user achievement and id

--- src/kms/api.rs
+++ src/kms/api.rs
@@ -1,15 +1,43 @@
-use crate::{api::MaplestoryApi, kms::character::character_list::CharacterList};
+use crate::{
+    api::MaplestoryApi,
+    kms::character::{achievement, character, character_list},
+};
 
-const X_NXOPEN_API_KEY: &str = "x-nxopen-api-key";
+const API_KEY_HEADER_NAME: &str = "x-nxopen-api-key";
 
-pub async fn get_character_list(api: &MaplestoryApi) -> CharacterList {
-    let url =
-        reqwest::Url::parse_with_params(&api.origin, [(X_NXOPEN_API_KEY, &api.api_key)]).unwrap();
-
-    reqwest::get(url)
+pub async fn get_character_list(api: &MaplestoryApi) -> character_list::CharacterList {
+    reqwest::Client::new()
+        .get(&format!("{}/maplestory/v1/character/list", &api.origin))
+        .header(API_KEY_HEADER_NAME, &api.api_key)
+        .send()
         .await
         .unwrap()
-        .json::<CharacterList>()
+        .json::<character_list::CharacterList>()
+        .await
+        .unwrap()
+}
+
+pub async fn get_user_achievement(api: &MaplestoryApi) -> achievement::Achievement {
+    reqwest::Client::new()
+        .get(&format!("{}/maplestory/v1/id", &api.origin))
+        .header(API_KEY_HEADER_NAME, &api.api_key)
+        .send()
+        .await
+        .unwrap()
+        .json::<achievement::Achievement>()
+        .await
+        .unwrap()
+}
+
+pub async fn get_id(api: &MaplestoryApi, character_name: String) -> character::Character {
+    reqwest::Client::new()
+        .get(&format!("{}/maplestory/v1/id", &api.origin))
+        .header(API_KEY_HEADER_NAME, &api.api_key)
+        .query(&[("character_name", character_name)])
+        .send()
+        .await
+        .unwrap()
+        .json::<character::Character>()
         .await
         .unwrap()
 }
