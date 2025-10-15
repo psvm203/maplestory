1c4532c35fe0dcc200e8c6a59d5b7722d278b4c4
psvm203 <psvm203@proton.me>
2025-10-15 22:27:56 +0900

    chore: clarify api error enums name

--- src/error.rs
+++ src/error.rs
@@ -5,7 +5,7 @@ pub enum ApiError {
     #[serde(rename = "OPENAPI00001")]
     InternalServerError,
     #[serde(rename = "OPENAPI00002")]
-    Forbidden,
+    UnauthorizedAccess,
     #[serde(rename = "OPENAPI00003")]
     InvalidIdentifier,
     #[serde(rename = "OPENAPI00004")]
@@ -15,11 +15,11 @@ pub enum ApiError {
     #[serde(rename = "OPENAPI00006")]
     InvalidPath,
     #[serde(rename = "OPENAPI00007")]
-    TooManyRequests,
+    LimitExceeded,
     #[serde(rename = "OPENAPI00009")]
-    DataNotReady,
+    DataBeingPrepared,
     #[serde(rename = "OPENAPI00010")]
-    GameUnderMaintenance,
+    ServiceUnderMaintenance,
     #[serde(rename = "OPENAPI00011")]
     ApiUnderMaintenance,
     SendRequestError,
