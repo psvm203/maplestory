baeade03ba83b12a961a15aff0bce4511556cbb9
psvm203 <psvm203@proton.me>
2025-10-20 23:10:04 +0900

    chore: derive Eq attributes for api error

--- src/error.rs
+++ src/error.rs
@@ -1,7 +1,7 @@
 use serde::Deserialize;
 use std::fmt::Display;
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Eq, PartialEq)]
 pub enum ApiError {
     #[serde(rename = "OPENAPI00001")]
     InternalServerError,
