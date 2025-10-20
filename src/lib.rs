39b2c6e09bed15afdc9bfe64d5175c18c5531b82
psvm203 <psvm203@proton.me>
2025-10-20 23:22:47 +0900

    docs: add crate descriptions

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,8 @@
+//! # maplestory
+//! maplestory API wrapper
+//!
+//! For more information, see [github.com/psvm203/maplestory](https://github.com/psvm203/maplestory).
+
 pub mod api;
 pub mod error;
 pub mod prelude;
