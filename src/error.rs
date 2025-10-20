90a6fc341c23e6ede2a6d31784cae68491a5157f
psvm203 <psvm203@proton.me>
2025-10-20 23:29:46 +0900

    chore: use error directly in the format! string

--- src/error.rs
+++ src/error.rs
@@ -30,6 +30,6 @@ pub enum ApiError {
 
 impl Display for ApiError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{:?}", self)
+        write!(f, "{self:?}")
     }
 }
